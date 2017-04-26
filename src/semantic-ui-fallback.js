a02b76c92937503783b8cf237064da31ff293b5b
copydog <775150@qq.com>
2017-04-26 19:49:01 -0400

    fixing equal width column

--- src/semantic-ui-fallback.js
+++ src/semantic-ui-fallback.js
@@ -1,3 +1,15 @@
 //******************** fix equal width grid ********************
-var el = $('.ui.equal.width.grid');
-var count = el.length;
+var number = [
+    'zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven',
+    'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen'
+];
+function loadEqualWidthGrid() {
+    var el = $('.ui.equal.width.grid');
+    var count = el.children().length;
+    var width = 100/count;
+
+    el.find('>.column').css('width', width+'%');
+}
+
+
+loadEqualWidthGrid();
--- tests/tester.js
+++ tests/tester.js
@@ -1,9 +1,11 @@
 $(document).ready(function () {
    $('#add').click(function () {
        $('head').append('<link rel="stylesheet" class="semantic-ui-fallback-css" type="text/css" href="../src/semantic-ui-fallback.css">');
+       $('body').append('<script class="semantic-ui-fallback-js" src="../src/semantic-ui-fallback.js"></script>');
    });
 
    $('#remove').click(function () {
       $('.semantic-ui-fallback-css').remove();
+      $('.semantic-ui-fallback-js').remove();
    });
 });
