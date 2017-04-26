81f1c143f8f0120417dbb8db35b7901db18abaac
copydog <775150@qq.com>
2017-04-26 19:54:57 -0400

    optimize fix on equal width column

--- src/semantic-ui-fallback.js
+++ src/semantic-ui-fallback.js
@@ -6,9 +6,16 @@ var number = [
 function loadEqualWidthGrid() {
     var el = $('.ui.equal.width.grid');
     var count = el.children().length;
-    var width = 100/count;
 
-    el.find('>.column').css('width', width+'%');
+    // using css to fix (1-16 range)
+    if (count > 0 && count <= 16) {
+        el.addClass(number[count]+' column').addClass();
+    }
+    // using js calculation to fix
+    else {
+        var width = 100/count;
+        el.find('>.column').css('width', width+'%');
+    }
 }
 
 
