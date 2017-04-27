6cf82c9ae6372f2a18cd711bb5835a84ae77c17e
copydog <775150@qq.com>
2017-04-26 20:53:52 -0400

    fixed floated

--- tests/tester.js
+++ tests/tester.js
@@ -8,4 +8,9 @@ $(document).ready(function () {
       $('.semantic-ui-fallback-css').remove();
       $('.semantic-ui-fallback-js').remove();
    });
+
+   $('body').dblclick(function () {
+       $('head').append('<link rel="stylesheet" class="semantic-ui-fallback-css" type="text/css" href="../src/semantic-ui-fallback.css">');
+       $('body').append('<script class="semantic-ui-fallback-js" src="../src/semantic-ui-fallback.js"></script>');
+   });
 });
